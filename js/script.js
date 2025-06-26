374108b028f937bcc706fbdc602b9ab771993c03
kavipatel001 <162306259+kavipatel001@users.noreply.github.com>
2025-06-26 14:11:32 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,18 +16,28 @@ function formatSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
+    let linked = await fetch(`http://127.0.0.1:3000/${currFolder}/`);
+    let response = await linked.text();
 
-    let linked = await fetch(`songs/${currFolder}/songs.json`);
-    let response = await linked.json();
-    songs = response.songs;
+    let div1 = document.createElement("div");
+    div1.innerHTML = response;
+    let as = div1.getElementsByTagName("a");
+    songs = [];
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split(`/${currFolder}/`)[1]);
+        }
+    }
 
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
     songUL.innerHTML = "";
     for (const song of songs) {
         songUL.innerHTML += `<li>
-                            <img class="invert" src="img/music.svg" alt="musicsvg">
+                            <img class="invert list-play-icon" src="img/play.svg" alt="playsvg" width="20px" height="20px">
                             <div class="info">
-                                <div>${song.replaceAll("-", " ")}</div>
+                                <div>${song.replaceAll("%20", " ")}</div>
                                 <div>Kavi</div>
                             </div>
                             <div class="playNow">
@@ -53,7 +63,7 @@ async function getSongs(folder) {
 }
 
 const playMusic = (tracking, pause = false, clickedElement = null) => {
-    currentSong.src = `songs/${currFolder}/` + tracking;
+    currentSong.src = `/${currFolder}/` + tracking;
 
     if (!pause) {
         currentSong.play();
@@ -72,25 +82,34 @@ const playMusic = (tracking, pause = false, clickedElement = null) => {
 };
 
 async function displayAlbums() {
-    let albums = ["frt", "sapphire"]; // Manually list your album folders here
-
+    let linked = await fetch(`http://127.0.0.1:3000/songs/`);
+    let response = await linked.text();
+    let div1 = document.createElement("div");
+    div1.innerHTML = response;
+    let anchor = div1.getElementsByTagName("a");
     let cardContainer = document.querySelector(".cardContainer");
+    let array = Array.from(anchor);
 
-    for (let folder of albums) {
-        let linked = await fetch(`songs/${folder}/info.json`);
-        let response = await linked.json();
+    for (let index = 0; index < array.length; index++) {
+        const e = array[index];
 
-        cardContainer.innerHTML += `<div data-folder="${folder}" class="card">
+        if (e.href.includes("/songs")) {
+            let folder = e.href.split("/").slice(-2)[0];
+
+            let linked = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`);
+            let response = await linked.json();
+            cardContainer.innerHTML += `<div data-folder="${folder}" class="card">
                         <div class="play">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
                                 <circle cx="12" cy="12" r="12" fill="#1DB954" />
                                 <path d="M9.5 11.1998V12.8002C9.5 14.3195 9.5 15.0791 9.95576 15.3862C10.4115 15.6932 11.0348 15.3535 12.2815 14.6741L13.7497 13.8738C15.2499 13.0562 16 12.6474 16 12C16 11.3526 15.2499 10.9438 13.7497 10.1262L12.2815 9.32594C11.0348 8.6465 10.4115 8.30678 9.95576 8.61382C9.5 8.92086 9.5 9.6805 9.5 11.1998Z" fill="#000000" />
                             </svg>
                         </div>
-                        <img src="songs/${folder}/cover.jpg" alt="image1">
+                        <img src="/songs/${folder}/cover.jpg" alt="image1">
                         <h2>${response.title}</h2>
                         <p>${response.dec}</p>
                     </div>`;
+        }
     }
 
     Array.from(document.getElementsByClassName("card")).forEach((e) => {
@@ -165,9 +184,6 @@ async function main() {
 
     document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e) => {
         currentSong.volume = parseInt(e.target.value) / 100;
-        if (currentSong.volume > 0) {
-            document.querySelector(".volume > img").src = document.querySelector(".volume > img").src.replace("img/mute.svg", "img/volume.svg");
-        }
     });
 
     document.querySelector(".volume > img").addEventListener("click", (e) => {
@@ -182,12 +198,15 @@ async function main() {
         }
     });
 
+    // ✅ Auto-next song on end
     currentSong.addEventListener("ended", () => {
         let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0]);
         if ((index + 1) < songs.length) {
+            // Play next song automatically
             let listItem = document.querySelector(".songList").getElementsByTagName("ul")[0].getElementsByTagName("li")[index + 1];
             playMusic(songs[index + 1], false, listItem);
         } else {
+            // Last song, stop
             play.src = "img/play.svg";
             if (currentListItem) {
                 currentListItem.querySelectorAll(".list-play-icon").forEach(img => img.src = "img/play.svg");
